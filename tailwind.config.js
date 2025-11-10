c07083db918e2c298b5ab2fcf8c63703a4f7b307
mirkogalizia <mirko.galizia@outlook.com>
2025-11-10 16:47:48 +0100

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
-theme: {
-  extend: {
-    fontFamily: {
-      'sans': ['var(--font-geist-sans)', 'Geist', 'Arial', 'sans-serif'],
-      'mono': ['var(--font-geist-mono)', 'Geist Mono', 'monospace'],
-    },
-    colors: {
-      pastelBlue: "#b3c7ff",
-      pastelPink: "#ffb3d6",
-      pastelPurple: "#e0bbff",
-      pastelTeal: "#b3fff0",
-    },
-  }
-}
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "Geist", "Arial", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "Geist Mono", "monospace"]
+      },
+      colors: {
+        pastelBlue: "#b3c7ff",
+        pastelPink: "#ffb3d6",
+        pastelPurple: "#e0bbff",
+        pastelTeal: "#b3fff0"
+      }
+    }
+  },
+  plugins: []
+};
